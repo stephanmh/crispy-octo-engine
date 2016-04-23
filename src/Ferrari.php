691fa9840497c3cfee7c644551f1146e22b16290
stephan marc hansen <stephanmh@gmail.com>
2016-04-23 23:45:58 +0200

    Added ferrari

---
+++ src/Ferrari.php
@@ -0,0 +1,18 @@
+<?php
+  /**
+   * User: stephan
+   * Date: 23-04-16
+   * Time: 23:45
+   */
+
+  namespace CrispyOcto;
+
+
+  /**
+   * Class Ferrari
+   *
+   * @package CrispyOcto
+   */
+  class Ferrari extends Car{
+
+  }
